452177c48b2b39b6b530cb2e55a8c78c8c980d9e
EmilyLiu220 <iaas0111@gmail.com>
2020-10-29 16:07:43 +0800

    revise is To fi timerTask

--- src/com/front_inform/timer/Timer_IsToFi.java
+++ src/com/front_inform/timer/Timer_IsToFi.java
@@ -5,14 +5,12 @@ import java.util.List;
 import java.util.TimerTask;
 import com.front_inform.model.*;
 import com.inform_set.model.*;
-import com.mem.model.*;
 
 // 每日掃 Inform_Set table，Query 當日通知設定，insert 至 Front_Inform 表格中(To 所有 Member)
 public class Timer_IsToFi extends TimerTask {
 	
 	private Front_InformService fiSvc = new Front_InformService();
 	private Inform_SetService isSvc = new Inform_SetService();
-	private MemService memSvc = new MemService();
 	
 	public Timer_IsToFi() { 
 		super(); 
@@ -24,21 +22,8 @@ public class Timer_IsToFi extends TimerTask {
 		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
 		String today = fmt.format(now);
 		List<Inform_SetVO> isVOs = isSvc.getIsByDate(today, today); // 取得當日通知 VO
-		List<MemVO> memVOs = memSvc.getAll(); // 取得所有會員 VO
 		for( Inform_SetVO isVO : isVOs ) { // 其實每天只會有一則
-			if(today.equals(isVO.getIs_date().toString())) {
-				String is_cont = isVO.getIs_cont();
-				for(MemVO memVO : memVOs) {
-					String mem_no = memVO.getMem_no(); // 取得會員編號
-					
-					
-					/***** 待修改成 batch insert 版 *****/
-					fiSvc.addNormalFI(mem_no, is_cont);
-					
-					
-					
-				}
-			}
+			fiSvc.addISToAll(isVO.getIs_no());
 		}
 	}
 	
